89a6528736f8b68c13fc53764bf11c8bf9f0bb05
acathers <37058209+acathers@users.noreply.github.com>
2019-02-27 02:50:38 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class Coke extends React.Component {
 
   render() {
     return (
-        <h1>Coke</h1>
-  );
+        <Container textAlign="center">
+          <Header as='h1'>Coke!</Header>
+        </Container>
+    );
   }
 }
 
-ReactDOM.render(<Coke />, document.getElementById('root'));
+ReactDOM.render(<Coke/>, document.getElementById('root'));
